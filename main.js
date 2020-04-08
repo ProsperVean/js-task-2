3617e572a9cc5535b2daca5d65bf19d506cf225e
Prosper Vean <prospervean@gmail.com>
2020-04-08 11:51:29 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+let rate;
+const interestCalculator = (array) => {
+    const interestData = array.map(item => {
+        const {
+           principal,
+           time
+           } = item;
+           if (principal >= 2500 && time >= 1 && time < 3) {
+            rate = 3;
+           } else if (principal >= 2500 && time >= 3) {
+            rate = 4;
+           } else if (principal < 2500 || time <= 1) {
+            rate = 2;
+           } else {
+            rate = 1;  
+           }
+
+           const interest = (principal * rate * time) / 100;
+
+           item.interest = interest;
+           item.rate = rate;
+
+           return item;
+    });
+
+    return interestData;  
+}
+
+const data = [{
+    principal: 2500,
+    time: 1.8
+},
+{
+    principal: 1000,
+    time: 5
+},
+{
+    principal: 3000,
+    time: 1
+},
+{
+    principal: 2000,
+    time: 3
+}
+];
+console.log(interestCalculator(data));
